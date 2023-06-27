6124e8e891edefa3b7221f602145e33505bca2a4
Muzammil-khan710 <khanmuzammil549@gmail.com>
2023-06-27 15:20:27 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const { withFaust, getWpHostname } = require('@faustwp/core');
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    webVitalsAttribution: ["CLS", "LCP", "FCP", "FID", "INP", "TTFB"],
+  },
+  sassOptions: {
+    includePath: [path.join(__dirname, "styles")],
+  },
+  images: {
+    domains: ["blog.rovae.in"]
+  },
+};
 
-module.exports = nextConfig
+module.exports = withFaust(nextConfig);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import { FaustProvider } from '@faustwp/core'
 import 'components/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import '../../faust.config'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const router = useRouter()
+
+  return (
+    <FaustProvider pageProps={pageProps} key={router.asPath}>
+      <Component {...pageProps} />
+    </FaustProvider>
+  )
+
 }
